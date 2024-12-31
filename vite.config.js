bc3ce529e2d11ea6078ad1bcdb807b5448fbb220
= <=>
2024-12-31 19:15:35 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base:"/dictionary-new/",
+  build: {
+    outDir: 'build', // Change 'build' to your desired folder name
+  },
   plugins: [react()],
 })
