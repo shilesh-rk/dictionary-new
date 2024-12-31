621288c870051a6784e37e9d2a68518b2c32f946
= <=>
2024-12-31 19:31:50 +0530

    deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base:"/dictionary-new/",
-  build: {
-    outDir: 'build', // Change 'build' to your desired folder name
-  },
   plugins: [react()],
 })
